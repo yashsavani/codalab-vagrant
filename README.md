1e6a689fd861fa0def4da89681df70b3fa70c3b8
yashsavani <yash.savani@gmail.com>
2016-09-03 22:22:08 -0700

    Some bug fixes.

--- README.md
+++ README.md
@@ -6,4 +6,15 @@ Vagrantfile and provisioning scripts to run a developer version of Codalab Works
 2. `git clone https://github.com/yashsavani/codalab-vagrant`
 3. `cd codalab-vagrant`
 4. `vagrant up`
-5. Visit http://localhost:4567
+5. `vagrant ssh`
+6. `cd /vagrant/codalab-worksheets && ./setup.sh`
+7. `cd /vagrant/codalab-cli && codalab/bin/cl config server/engine_url mysql://codalab:password@localhost:3306/codalab_bundles`
+8. `scripts/create-root-user.py password`
+9. `echo -e "codalab\npassword" > /home/vagrant/.codalab/root.password`
+10. `chmod 600 /home/vagrant/.codalab/root.password`
+11. `cd /vagrant/codalab-worksheets/codalab && screen -dmS website ./manage runserver 127.0.0.1:2700`
+12. `cd /vagrant/codalab-cli && screen -dmS XMLRPC codalab/bin/cl server`
+13. `screen -dmS REST codalab/bin/cl rest-server`
+14. `screen -dmS bundle_manager codalab/bin/cl bundle_manager`
+15. `cd /vagrant && screen -dmS worker codalab-cli/worker/worker.sh --server http://localhost:2900 --password $CODALAB_HOME/root.password`
+16. Visit http://localhost:4567
--- bootstrap.sh
+++ bootstrap.sh
@@ -37,21 +37,16 @@ git clone "https://github.com/codalab/codalab-cli" "$HOME/codalab-cli"
 
 echo -e "\n--- Running setup in both repositories ---\n"
 cd $HOME/codalab-worksheets && ./setup.sh 
-# Above line doesn't run npm
-cd codalab/apps/web && npm install && npm run bower && npm run build &&
-chmod a+rx . && chmod a+rx codalab && chmod a+rx codalab/apps && 
-chmod a+rx codalab/apps/web && chmod -R a+r codalab/apps/web/static && 
-find codalab/apps/web/static -type d -exec chmod a+x {} \;
-
+# Above line doesn't install npm packages
 cd $HOME/codalab-cli && ./setup.sh server
 
-echo -e "\n--- Configuring CodaLab to use Database ---\n"
-cd $HOME/codalab-cli && codalab/bin/cl config server/engine_url mysql://$DBUSER:$DBPASSWD@$DBHOST:3306/$DBNAME
+#echo -e "\n--- Configuring CodaLab to use Database ---\n"
+#cd $HOME/codalab-cli && codalab/bin/cl config server/engine_url mysql://$DBUSER:$DBPASSWD@$DBHOST:3306/$DBNAME
 
-echo -e "\n--- Configuring the workers ---\n"
-cd $HOME/codalab-cli && scripts/create-root-user.py $DBPASSWD
-echo -e "$DBUSER\n$DBPASSWD" > $CODALAB_HOME/root.password
-chmod 600 $CODALAB_HOME/root.password
+#echo -e "\n--- Configuring the workers ---\n"
+#cd $HOME/codalab-cli && scripts/create-root-user.py $DBPASSWD
+#echo -e "$DBUSER\n$DBPASSWD" > $CODALAB_HOME/root.password
+#chmod 600 $CODALAB_HOME/root.password
 
 echo -e "\n--- Installing Nginx ---\n"
 apt-get -y install nginx
@@ -61,13 +56,9 @@ cd $HOME/codalab-worksheets/codalab && ./manage config_gen
 cd $HOME/codalab-worksheets/codalab && ln -sf $PWD/config/generated/nginx.conf /etc/nginx/sites-enabled/codalab.conf
 service nginx restart
 
-echo -e "\n--- Setting up Environment Variables ---\n"
-export HOME=$HOME
-export CODALAB_HOME=$CODALAB_HOME
-
-echo -e "\n--- Setting up Screens ---\n"
-cd $HOME/codalab-worksheets/codalab && screen -dmS website./manage runserver 127.0.0.1:2700
-cd $HOME/codalab-cli && screen -dmS XMLRPC codalab/bin/cl server
-cd $HOME/codalab-cli && screen -dmS REST codalab/bin/cl rest-server
-cd $HOME/codalab-cli && screen -dmS bundle_manager codalab/bin/cl bundle-manager
-cd $HOME && screen -dmS worker codalab-cli/worker/worker.sh --server http://localhost:2900 --password $CODALAB_HOME/root.password
+#echo -e "\n--- Setting up Screens ---\n"
+#cd $HOME/codalab-worksheets/codalab && screen -dmS website ./manage runserver 127.0.0.1:2700
+#cd $HOME/codalab-cli && screen -dmS XMLRPC codalab/bin/cl server
+#cd $HOME/codalab-cli && screen -dmS REST codalab/bin/cl rest-server
+#cd $HOME/codalab-cli && screen -dmS bundle_manager codalab/bin/cl bundle-manager
+#cd $HOME && screen -dmS worker codalab-cli/worker/worker.sh --server http://localhost:2900 --password $CODALAB_HOME/root.password
